d09d597fcf8d7d36c25c295db6a72b144c604ee2
Gabriel-CSS <gabriel.silva.gs142@gmail.com>
2021-07-07 19:36:55 -0300

    feat: criado metodo calculaQuantidadeAplicarTonHa
    - Criado o metodo para calcular a quantidade a ser aplicada em toneladas
      por hectares (Ton/ha)

--- src/main/java/edu/utfpr/cp/dacom/sa/soilcorrection/CorrecaoCalcioMagnesio.java
+++ src/main/java/edu/utfpr/cp/dacom/sa/soilcorrection/CorrecaoCalcioMagnesio.java
@@ -1,5 +1,17 @@
 package edu.utfpr.cp.dacom.sa.soilcorrection;
 
 public class CorrecaoCalcioMagnesio implements ICorrecaoNutriente<FonteCalcioMagnesio> {
+    
+    public double calculaQuantidadeAplicarTonHa(double qtdeCorretivoAplicar, double prnt) {
+        
+        if (prnt <= 0) {
+            throw new IllegalArgumentException();
+        }
+        
+        if (qtdeCorretivoAplicar <= 0) {
+            throw new IllegalArgumentException();
+        }
 
+        return qtdeCorretivoAplicar * 100 / prnt;
+    }
 }
