347cbb1094f7de4b2b8eb43917818f728a2e6079
Gabriel-CSS <gabriel.silva.gs142@gmail.com>
2021-08-02 15:43:32 -0300

    feat: Adicionando a biblioteca lombok ao projeto

--- src/test/java/edu/utfpr/cp/dacom/sa/soilcorrection/TestaCorrecaoCalcioMagnesio.java
+++ src/test/java/edu/utfpr/cp/dacom/sa/soilcorrection/TestaCorrecaoCalcioMagnesio.java
@@ -1,6 +1,6 @@
 package edu.utfpr.cp.dacom.sa.soilcorrection;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
