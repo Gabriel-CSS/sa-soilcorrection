73ba86edd37fd7a71559f9fcd02a08d6af605f05
Gabriel-CSS <gabriel.silva.gs142@gmail.com>
2021-08-03 10:48:24 -0300

    refact: substituindo anotacoes globais para cada atributo
    - Substituido o uso global das anotacoes Getter e Setter pelo uso
      nos atributos em que se fazem necessarios, evitando que o atributo
      "nome" possua getter e setter desnecessarios.

--- src/main/java/edu/utfpr/cp/dacom/sa/soilcorrection/NutrienteAdicional.java
+++ src/main/java/edu/utfpr/cp/dacom/sa/soilcorrection/NutrienteAdicional.java
@@ -4,12 +4,14 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-@Getter @Setter
 @RequiredArgsConstructor
 public class NutrienteAdicional {
 
     private final NomeNutrienteAdicional nome;
+    
+    @Getter
     private final double teorNutriente;
-
+    
+    @Getter @Setter
     private double correcaoAdicional;
 }
